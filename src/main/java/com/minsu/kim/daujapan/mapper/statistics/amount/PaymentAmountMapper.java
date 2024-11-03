fd7b66639eb5510c1fd5b891f01cfb50f4f381ae
Minsu.Kim <kds3335k@icloud.com>
2024-11-03 14:12:17 +0900

    [20241103] mapper bean 타입 추가 1.  mapper bean 타입 추가

--- src/main/java/com/minsu/kim/daujapan/mapper/statistics/amount/PaymentAmountMapper.java
+++ src/main/java/com/minsu/kim/daujapan/mapper/statistics/amount/PaymentAmountMapper.java
@@ -1,21 +1,22 @@
 package com.minsu.kim.daujapan.mapper.statistics.amount;
 
-import com.minsu.kim.daujapan.data.statistics.amount.PaymentAmountRecord;
-import com.minsu.kim.daujapan.domains.statistics.amount.PaymentAmountStatisticEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
+import com.minsu.kim.daujapan.data.statistics.amount.PaymentAmountRecord;
+import com.minsu.kim.daujapan.domains.statistics.amount.PaymentAmountStatisticEntity;
+
 /**
- *
  * @author minsu.kim
  * @since 1.0
  */
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PaymentAmountMapper {
 
-    PaymentAmountMapper INSTANCE =  Mappers.getMapper(PaymentAmountMapper.class);
+  PaymentAmountMapper INSTANCE = Mappers.getMapper(PaymentAmountMapper.class);
 
-    PaymentAmountRecord entityToDto(PaymentAmountStatisticEntity entity);
-    PaymentAmountStatisticEntity dtoToEntity(PaymentAmountRecord paymentAmountRecord);
+  PaymentAmountRecord entityToDto(PaymentAmountStatisticEntity entity);
+
+  PaymentAmountStatisticEntity dtoToEntity(PaymentAmountRecord paymentAmountRecord);
 }
--- src/main/java/com/minsu/kim/daujapan/mapper/statistics/amount/SalesAmountMapper.java
+++ src/main/java/com/minsu/kim/daujapan/mapper/statistics/amount/SalesAmountMapper.java
@@ -1,21 +1,22 @@
 package com.minsu.kim.daujapan.mapper.statistics.amount;
 
-import com.minsu.kim.daujapan.data.statistics.amount.SalesAmountRecord;
-import com.minsu.kim.daujapan.domains.statistics.amount.SalesAmountStatisticEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
+import com.minsu.kim.daujapan.data.statistics.amount.SalesAmountRecord;
+import com.minsu.kim.daujapan.domains.statistics.amount.SalesAmountStatisticEntity;
+
 /**
- *
  * @author minsu.kim
  * @since 1.0
  */
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SalesAmountMapper {
 
-    SalesAmountMapper INSTANCE =  Mappers.getMapper(SalesAmountMapper.class);
+  SalesAmountMapper INSTANCE = Mappers.getMapper(SalesAmountMapper.class);
 
-    SalesAmountRecord entityToDto(SalesAmountStatisticEntity entity);
-    SalesAmountStatisticEntity dtoToEntity(SalesAmountRecord salesAmountRecord);
+  SalesAmountRecord entityToDto(SalesAmountStatisticEntity entity);
+
+  SalesAmountStatisticEntity dtoToEntity(SalesAmountRecord salesAmountRecord);
 }
