e538d010b5e775c711248004ce66da415c799762
Minsu.Kim <kds3335k@icloud.com>
2024-11-04 02:28:37 +0900

    [20241104] 모든 요청별 request, response 타임스탬프 로그 남기기 외 1건 1. 모든 요청별 request, response 타임스탬프 로그 남기기 2. README.md 작성

--- src/main/java/com/minsu/kim/daujapan/config/WebConfig.java
+++ src/main/java/com/minsu/kim/daujapan/config/WebConfig.java
@@ -1,14 +1,25 @@
 package com.minsu.kim.daujapan.config;
 
+import com.minsu.kim.daujapan.interceptors.RequestProcessTimeMarkerInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+  private final RequestProcessTimeMarkerInterceptor timeMarkerInterceptor;
 
   @Override
   public void configurePathMatch(PathMatchConfigurer configurer) {
     configurer.setUseTrailingSlashMatch(true);
+  }
+
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(timeMarkerInterceptor);
   }
 }
